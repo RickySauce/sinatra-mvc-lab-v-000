d0948a65094466fb99f5ee57547ef7ba75bef241
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-04 12:24:49 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,7 +7,7 @@ class PigLatinizer
     text = text.split("")
     letters = []
     text.each do |letter|
-      vowels.any? {|vowel| vowel == letter}
+      letters << letter until vowels.any? {|vowel| vowel == letter}
       binding.pry
     end
   end
