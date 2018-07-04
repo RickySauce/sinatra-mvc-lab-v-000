a056d924f9ffb140b64640ce49936de1acfa35da
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-04 12:33:29 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,6 +5,7 @@ class PigLatinizer
   def piglatinize(text)
     vowels = ["a","e","i","o","u"]
     text = text.split("")
+    new_text = text
     letters = []
     text.each do |letter|
       if vowels.any?{|vowel| vowel == letter}
