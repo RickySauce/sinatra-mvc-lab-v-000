d6911cabd9cf2098b3995f591c9e1b840e3b5942
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-04 13:08:28 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,32 +3,35 @@ require 'pry'
 class PigLatinizer
 
   def piglatinize(text)
-    vowels = ["a","e","i","o","u"]
-    new_text = text.downcase.split("")
-    letters = []
-    new_text.each do |letter|
-      if vowels.any?{|vowel| vowel == letter}
-        new_text.clear
-      else
-        letters << letter
+    text_array = text.split(" ")
+    text_array.each do |text|
+      vowels = ["a","e","i","o","u"]
+      new_text = text.downcase.split("")
+      letters = []
+      new_text.each do |letter|
+        if vowels.any?{|vowel| vowel == letter}
+          new_text.clear
+        else
+          letters << letter
+        end
       end
-    end
-    text = text.split("")
-    (letters.count).times {text.shift}
-    if letters.count == 0
-      if text.count < 4
-        letters << text.shift
-        text << letters << "way"
-        text = text.flatten.join
+      text = text.split("")
+      (letters.count).times {text.shift}
+      if letters.count == 0
+        if text.count < 4
+          letters << text.shift
+          text << letters << "way"
+          text = text.flatten.join
+        else
+          text << "way"
+          text = text.flatten.join
+        end
       else
-        text << "way"
+        text << letters << "ay"
         text = text.flatten.join
       end
-    else
-      text << letters << "ay"
-      text = text.flatten.join
+      text
     end
-    text
   end
 
 end
