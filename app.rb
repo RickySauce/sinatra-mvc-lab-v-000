ec3c5d0c7fc325391dd0f90d51e8cf12cebc0d16
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-04 13:45:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,9 +7,9 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @phrase = PigLatinizer.new
-    @phrase = @phrase.piglatinize(params["user_phrase"])
-    erb :piglatin
-  end
+   pl = PigLatinizer.new
+   @piglatin = pl.piglatinize(params[:user_phrase])
+   erb :results
+ end
 
 end
