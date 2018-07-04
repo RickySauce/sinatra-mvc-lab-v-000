61bb38b93d7aadb6be704fcd15b7df827ff1af87
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-04 12:54:50 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,7 +5,6 @@ class PigLatinizer
   def piglatinize(text)
     vowels = ["a","e","i","o","u"]
     text = text.split("")
-    binding.pry
     letters = []
     text.each do |letter|
       if vowels.any?{|vowel| vowel == letter}
@@ -17,6 +16,7 @@ class PigLatinizer
     text = text.split("")
     letters.each_with_index {|letter, index| text.delete_at(index)}
     text
+    binding.pry
   end
 
 end
